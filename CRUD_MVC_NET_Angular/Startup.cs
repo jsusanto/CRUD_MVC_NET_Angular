5b2a1b2743f3da101b948a50f614e4ba210076c6
Jeffry Susanto <jeffry.susanto@hotmail.com>
2018-10-10 14:15:51 +1100

    Empty MVC initial project

---
+++ CRUD_MVC_NET_Angular/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRUD_MVC_NET_Angular.Startup))]
+namespace CRUD_MVC_NET_Angular
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
