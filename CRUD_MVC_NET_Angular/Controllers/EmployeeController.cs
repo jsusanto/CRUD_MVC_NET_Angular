c5df428c92a7c6dbcf42c8b3dbf18160b4a66ad4
Jeffry Susanto <jeffry.susanto@hotmail.com>
2018-10-10 15:14:10 +1100

    Add module.js, service.js, controller.js for angular

---
+++ CRUD_MVC_NET_Angular/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_MVC_NET_Angular.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
